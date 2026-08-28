c058a4ddaf455f46eecfe3a2e296a2883b2dd9bb
prakhar10-python <prakhardhanbad@gmail.com>
2026-08-28 11:39:53 +0530

    CRUD operation on Place API via API Chaining

--- src/main/java/com/example/api/endpoints/PlaceApiEndPoints.java
+++ src/main/java/com/example/api/endpoints/PlaceApiEndPoints.java
@@ -4,5 +4,8 @@ public final class PlaceApiEndPoints {
 
     private PlaceApiEndPoints() {}
 
-    public static final String PLACE = "/maps/api/place/add/json";
+    public static final String ADD_PLACE = "/maps/api/place/add/json";
+    public static final String GET_PLACE = "/maps/api/place/get/json";
+    public static final String UPDATE_PLACE = "/maps/api/place/update/json";
+    public static final String DELETE_PLACE = "/maps/api/place/delete/json";
 }
--- src/test/java/com/example/api/tests/AddPlaceTest.java
+++ src/test/java/com/example/api/tests/AddPlaceTest.java
@@ -3,6 +3,9 @@ package com.example.api.tests;
 import com.example.api.base.CourseBaseTest;
 import com.example.api.config.ConfigReader;
 import com.example.api.endpoints.PlaceApiEndPoints;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -10,11 +13,15 @@ import static org.hamcrest.Matchers.*;
 
 
 public class AddPlaceTest extends CourseBaseTest {
-    //Validate add place Api
 
-    @Test
+    String placeId;
+    String newAddress = "Daffodils PG,123";
+
+    //Validate add place Api
+    // JSON Path is for the response parsing
+    @Test(priority = 1)
     public void addPlace() {
-        given().
+        placeId = given().
                 log().all()
                 .queryParam("key", ConfigReader.getCourseKey())
                 .header("Content-Type", ConfigReader.getContentType())
@@ -34,11 +41,59 @@ public class AddPlaceTest extends CourseBaseTest {
                         "  \"website\": \"http://google.com\",\n" +
                         "  \"language\": \"French-IN\"\n" +
                         "}")
-                .when().post(PlaceApiEndPoints.PLACE)
+                .when().post(PlaceApiEndPoints.ADD_PLACE)
                 .then().assertThat().statusCode(200)
-                .body("scope",equalTo("APP"))
+                .body("scope", equalTo("APP"))
+                .log().all()
+                .extract().response().jsonPath().getString("place_id");
+    }
+
+
+    //Validate update place
+    @Test(dependsOnMethods = "addPlace")
+    public void updatePlace() {
+       given()
+                .queryParam("key", ConfigReader.getCourseKey())
+                .header("Content-Type", ConfigReader.getContentType())
+                .body("{\n" +
+                        "  \"place_id\": \"" + placeId + "\",\n" +
+                        "  \"address\": \"" + newAddress + "\",\n" +
+                        "  \"key\": \"qaclick123\"\n" +
+                        "}")
+                .when().put(PlaceApiEndPoints.UPDATE_PLACE)
+                .then()
+                .assertThat()
+                .statusCode(200)
                 .log().all();
+    }
+
+    @Test(dependsOnMethods = "updatePlace")
+    public void getPlaceAfterUpdation() {
+        Response response = given().log().all()
+                .queryParam("key", ConfigReader.getCourseKey())
+                .queryParam("place_id", placeId)
+                .when().get(PlaceApiEndPoints.GET_PLACE)
+                .then().assertThat().statusCode(200)
+                .body("address", equalTo(newAddress)) //given when then asserstion
+                .log().all()
+                .extract()
+                .response();
+
+        JsonPath jsonPath = response.jsonPath();
+        String actualAddress = jsonPath.get("address");
+        Assert.assertEquals(newAddress,actualAddress); //assertion using java assertion library
 
     }
 
+    @Test(dependsOnMethods = "getPlaceAfterUpdation")
+    public void deletePlace() {
+        given()
+                .log().all()
+                .queryParam("key", ConfigReader.getCourseKey())
+                .queryParam("place_id", placeId)
+                .when().delete(PlaceApiEndPoints.DELETE_PLACE)
+                .then().assertThat().statusCode(200)
+                .log().all();
+
+    }
 }
